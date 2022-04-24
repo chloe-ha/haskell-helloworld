603ee1c9bdbc4b31a122ef2a0d8459ade112ad41
Chloe <pnchloeha@gmail.com>
2022-04-24 14:39:11 +1000

    Refactor firstStep method to lib

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,4 @@ import System.Environment
 main :: IO ()
 main = do
   args <- getArgs
-  putStrLn ("Hello " ++ head args ++ ", " ++ args !! 1 ++ " x " ++ args !! 2 ++ " = " ++ show (read (args !! 1) * read (args !! 2)))
-  putStrLn "Name a singer you like:"
-  singer <- getLine
-  putStrLn ("Yeah, " ++ singer ++ " is awesome")
+  firstSteps (head args, read (args !! 1), read (args !! 2))
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,12 @@
 module Lib
-  ( helloworld,
+  ( firstSteps,
   )
 where
 
-helloworld :: IO ()
-helloworld = putStrLn "Hello World"
+-- firstSteps stack run Josh 2 5
+firstSteps :: (String, Int, Int) -> IO ()
+firstSteps (name, n1, n2) = do
+  putStrLn ("Hello " ++ name ++ ", " ++ show n1 ++ " x " ++ show n2 ++ " = " ++ show (n1 * n2))
+  putStrLn "Name a singer you like:"
+  singer <- getLine
+  putStrLn ("Yeah, " ++ singer ++ " is awesome")
